49d618dfcc8d818c440ea3aad5f9ff65f3ab499d
Darien Lombardi <dlombardi@teamworks.com>
2019-05-17 16:00:37 -0400

    add api tests for exports from asyncOperationsManager and reduxIntegration

--- src/__tests__/api_test.js
+++ src/__tests__/api_test.js
@@ -8,4 +8,40 @@ describe('API', () => {
     expect(reduxIntegration).to.exist;
     expect(asyncOperationsManager).to.exist;
   });
+  it('should assert the correct library asyncOperationsManager API', () => {
+    expect(asyncOperationsManager).to.have.all.keys(
+      // CONFIG //
+      'initializeWithOptions',
+      'defaultLoggerOptions',
+
+      // State API //
+      'getAsyncOperationsManagerState',
+      'clearAsyncOperationsManagerState',
+      'setAsyncOperationsManagerState',
+
+      // Operation & Decriptor API //
+      'getAsyncOperation',
+      'invalidateAsyncOperation',
+      'getStateForOperationAfterStep',
+      'registerAsyncOperationDescriptors',
+      'shouldRunOperation',
+
+      // CONSTANTS //
+      'ASYNC_OPERATION_TYPES',
+      'ASYNC_OPERATION_STEPS',
+      'FETCH_STATUS',
+      'DATA_STATUS',
+    );
+  });
+  it('should assert the correct library reduxIntegration API', () => {
+    expect(reduxIntegration).to.have.all.keys(
+      'createAsyncOperationInitialAction',
+      'createAsyncOperationBeginAction',
+      'createAsyncOperationResolveAction',
+      'createAsyncOperationRejectAction',
+      'getAsyncOperationResolveActionType',
+      'asyncOperationReducer',
+      'getActionForAsyncOperation',
+    );
+  });
 });
